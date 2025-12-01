e467f6674dc88809814c0e5ae0415fabca623d81
John Mola <mj66005@mail.harpercollege.edu>
2025-11-30 21:53:30 -0600

    Fixed Main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
-
-#include "src/hello.hpp"
+#include "src/link.hpp"
+#include "src/node.hpp"
 
 int main() {
-  std::cout << hello() << std::endl;
+  Link gold = Link("Gold");
+  Node* node_one = new Node(&gold);
+
+  Link silver = Link("Silver");
+  Node* node_two = new Node(&silver, node_one);
+
+  Link bronze = Link("Bronze");
+  Node* node_three = new Node(&bronze, node_two);
+
+  Node* retrieved_node_two = node_one->next();
+  Node* retrieved_node_three = node_two->next();
+
+  std::cout << "Traversing the linked list:" << std::endl;
+  Node* current = node_one;
+  int position = 1;
+  while (current != nullptr) {
+    std::cout << "Node " << position << ": " << current->get_data()->get_material() << std::endl;
+    current = current->next();
+    position++;
+  }
+
+  std::cout << "\nVerification:" << std::endl;
+  std::cout << "retrieved_node_two contains: " << retrieved_node_two->get_data()->get_material() << std::endl;
+  std::cout << "retrieved_node_three contains: " << retrieved_node_three->get_data()->get_material() << std::endl;
+
+  delete node_one;
+  delete node_two;
+  delete node_three;
+
+  return 0;
 }
